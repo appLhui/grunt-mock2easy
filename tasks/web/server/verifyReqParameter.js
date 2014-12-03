92d37ec5fc199593fb4f8c918b7688096f9a6c2f
mofei <mofei.lh@alibaba-inc.com>
2014-12-03 09:44:54 +0800

    修改 接口检查错误，改正后不会修改的bug

--- tasks/web/server/verifyReqParameter.js
+++ tasks/web/server/verifyReqParameter.js
@@ -12,20 +12,17 @@ module.exports = function(grunt){
     var colors = require('colors');
 
     return function(url,method,params,jsonData,ignoreField){
-        var doWrite = false;
         jsonData.reqError = [];
         jsonData.docError = [];
         delete jsonData.methodError;
 
         if(method!==jsonData.interfaceType){
             jsonData.methodError ='请求类型异常请确认';
-            doWrite = true;
         }
 
         jsonData.requiredParameters.forEach(function(o){
           if(o.required && !!!params[o.name]){
             jsonData.reqError.push('请求参数［'+o.name+'］应该为必填项');
-            doWrite = true;
           }
         });
 
@@ -40,7 +37,6 @@ module.exports = function(grunt){
             });
             if(_i === jsonData.requiredParameters.length && ignoreField.indexOf(name) === -1){
                 jsonData.docError.push('接口文档缺少［'+ name +'］做为请求参数');
-                doWrite = true;
             }
         }
 
@@ -63,8 +59,6 @@ module.exports = function(grunt){
             }
             grunt.log.writeln('+-------------------------------------------------------'.red);
         }
-        if(doWrite){
-          fs.writeFileSync(path.resolve(global.options.database)+ url,JSON.stringify(jsonData,undefined,5));
-        }
+        fs.writeFileSync(path.resolve(global.options.database)+ url,JSON.stringify(jsonData,undefined,5));
     }
 };
